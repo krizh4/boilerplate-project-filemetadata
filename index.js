8fc8fbecbd8c663a939d3378fb4655be499b78cc
krizh4 <krishmikaabisheka1@gmail.com>
2022-09-17 17:20:45 +0530

    Sloved 1.0.0

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
 var express = require('express');
 var cors = require('cors');
+var multer  = require('multer');
 require('dotenv').config()
 
 var app = express();
+var uploads = multer({ dest: 'uploads/' });
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
+})
+
+app.post('/api/fileanalyse', uploads.single('upfile'), (req, res) => {
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  });
 });
-
-
-
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
